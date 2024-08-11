370028aeebac8965e8b9f03dda507e46bae705ca
HaizeaRL <haizearumayor@gmail.com>
2024-08-11 14:34:15 +0200

    Searching code removed

--- 2.1-Message_searching.R
+++
@@ -1,6 +0,0 @@
-if(Sys.getenv("R_ROOT") =="") Sys.setenv(R_ROOT=getwd())
-dir<-paste(Sys.getenv("R_ROOT"),"/2-Analysis_of_Tweets/",sep="")
-setwd(dir)
-source("GetTwitterData.R")
-setwd(Sys.getenv("R_ROOT"))
-
